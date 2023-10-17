586684a5d9b8104c6fb09e8a6a0af14b6c1bd2cb
dilrong <dilrong@dilrong.com>
2023-10-18 00:04:15 +0900

    feat: 목차 좌측정렬

--- src/app/components/BookIndex.tsx
+++ src/app/components/BookIndex.tsx
@@ -13,37 +13,39 @@ const BookIndex = () => {
           height={500}
         />
       </div>
-      <div className="flex flex-col gap-4 justify-center">
-        <h2 className="text-xl mb-2">목차</h2>
-        <p>
-          <span className="text-violet-700">1.</span> 들어가며
-        </p>
-        <p>
-          <span className="text-violet-700">2.</span> ENTJ 남자 종류 2가지
-        </p>
-        <p>
-          <span className="text-violet-700">3.</span> ENTJ 남자가 좋아하는 이성
-          스타일
-        </p>
-        <p>
-          <span className="text-violet-700">4.</span> ENTJ 남자를 어떻게?
-        </p>
-        <p>
-          <span className="text-violet-700">5.</span> ENTJ 남자를 공략하기 위한
-          스몰토크 주제
-        </p>
-        <p>
-          <span className="text-violet-700">6.</span> ENTJ 남자에게 고백하는
-          방법
-        </p>
-        <p>
-          <span className="text-violet-700">7.</span> ENTJ 남자와 연애할때
-          반드시 알아야 할점
-        </p>
-        <p>
-          <span className="text-violet-700">8.</span> 나가며
-        </p>
-      </div>
+      <section className="flex flex-col justify-center ">
+        <h2 className="text-xl mb-8">목차</h2>
+        <article className="flex flex-col gap-4 text-left">
+          <p>
+            <span className="text-violet-700">1.</span> 들어가며
+          </p>
+          <p>
+            <span className="text-violet-700">2.</span> ENTJ 남자 종류 2가지
+          </p>
+          <p>
+            <span className="text-violet-700">3.</span> ENTJ 남자가 좋아하는
+            이성 스타일
+          </p>
+          <p>
+            <span className="text-violet-700">4.</span> ENTJ 남자를 어떻게?
+          </p>
+          <p>
+            <span className="text-violet-700">5.</span> ENTJ 남자를 공략하기
+            위한 스몰토크 주제
+          </p>
+          <p>
+            <span className="text-violet-700">6.</span> ENTJ 남자에게 고백하는
+            방법
+          </p>
+          <p>
+            <span className="text-violet-700">7.</span> ENTJ 남자와 연애할때
+            반드시 알아야 할점
+          </p>
+          <p>
+            <span className="text-violet-700">8.</span> 나가며
+          </p>
+        </article>
+      </section>
     </div>
   );
 };
